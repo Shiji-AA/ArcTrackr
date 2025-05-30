8c4abd5288a3ad91f9b59c1ba012e2dd79501ee7
shijiaa <shijihcl@gmail.com>
2025-05-30 11:57:04 +0530

    Live link added

--- Frontend/src/api/axiosInstance.jsx
+++ Frontend/src/api/axiosInstance.jsx
@@ -2,7 +2,7 @@ import axios from "axios"
 
 const axiosInstanceAdmin = axios.create({
     baseURL:`${import.meta.env.VITE_PUBLIC_API_URL}/api/admin`
-     //baseURL: "https://castme-project-1.onrender.com/api/admin",
+   
   
 })
 
